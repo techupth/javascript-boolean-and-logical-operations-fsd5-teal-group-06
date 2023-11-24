833fbe8af11b340752ca6936895ce26cdb2fe737
mygit <vuttipat.rungrojbribun@gmail>
2023-11-24 14:29:59 +0700

    dear:assignment

--- ex-2.js
+++ ex-2.js
@@ -3,14 +3,15 @@
 // Start coding here
 let lastMonthPaidMoreThan4000 = true;
 let isWeekday = true;
-let hasNotBoughtProductFromITCategory = false;
-let hasNotAttendedDiscountEvent = true;
+let hasNotBoughtProductFromITCategory = true;
+let hasNotAttendedDiscountEvent = false;
 let isPlatinum = false;
 
 let hasPromotion =
   (lastMonthPaidMoreThan4000 &&
     isWeekday &&
-    hasNotBoughtProductFromITCategory & hasNotAttendedDiscountEvent) ||
+    hasNotBoughtProductFromITCategory &&
+    hasNotAttendedDiscountEvent) ||
   isPlatinum;
 
 console.log(hasPromotion);
---
+++ ex-3.js
@@ -0,0 +1,7 @@
+// Exercise #3: Code Debugging
+let numberA = 10;
+const numberB = "20";
+
+numberA = 10;
+
+console.log(`Sum: ${numberA + Number(numberB)}`);
