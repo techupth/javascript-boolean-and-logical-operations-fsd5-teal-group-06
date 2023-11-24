7b3c203710006662160fe34efff3861cb1844f28
Sodikcp <paramesh.rah@gmail.com>
2023-11-24 13:10:09 +0700

    feat:complete exercise logical

--- ex-1.js
+++ ex-1.js
@@ -2,8 +2,8 @@
 
 // Start coding here
 let isOver18 = true;
-let hasNoCriminalBlacklist = true;
+let hasCriminalBlacklist = false;
 let isThai = false;
 
-let isAllow = (isOver18 && hasNoCriminalBlacklist) || isThai;
+let isAllow = (isOver18 && !hasCriminalBlacklist) || isThai
 console.log(isAllow);
---
+++ ex-3.js
@@ -0,0 +1,7 @@
+ // Exercise #3: Code Debugging
+ let numberA = 100;
+ const numberB = "20";
+
+ numberA = 10;
+
+ console.log(`Sum: ${numberA + Number(numberB)}`);
